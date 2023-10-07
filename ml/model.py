d871b52030a553dbad8b63ff561a23925bfc5564
mishra-alla <126316603+mishra-alla@users.noreply.github.com>
2023-10-07 10:05:36 +0300

    Update model.py

--- ml/model.py
+++ ml/model.py
@@ -1,10 +1,22 @@
+import pandas as pd
+import numpy as np
+import pandas as pd
+import pickle
+
 def forecast(sales: dict, item_info: dict, store_info: dict) -> list:
     """
     Функция для предсказания продажЖ
     :params sales: исторические данные по продажам
     :params item_info: характеристики товара
     :params store_info: характеристики магазина
+    """
+     
+    # Загрузка модели
+    try:
+        with open('app/pickle_model/model.pcl', 'rb') as fid:
+            model = pickle.load(fid)
 
+    
     """
     sales = [el["sales_units"] for el in sales]
     mean_sale = sum(sales) / len(sales)
